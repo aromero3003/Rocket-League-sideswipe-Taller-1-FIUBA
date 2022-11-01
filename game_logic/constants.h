80d62d1febf041836db760ed905c9ff7ba647d11
aromero3003 <alromero@fi.uba.ar>
2022-11-01 03:22:48 -0300

    feat: archivo para mantener todas las constantes del programa

---
+++ game_logic/constants.h
@@ -0,0 +1,14 @@
+#ifndef __CONSTANTS_H__
+#define __CONSTANTS_H__
+
+// Recommended iterations for constraint resolver
+#define VELOCITY_ITERATIONS 8
+#define POSITION_ITERATIONS 3
+
+#define TIME_STEP 1.0f/60.f
+
+
+// Ball
+#define BALL_RADIUS 1.0f
+
+#endif
