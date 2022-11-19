f99cd5b11571c4222a0446febbda3b36327c9c46
aromero3003 <alromero@fi.uba.ar>
2022-11-19 03:00:39 -0300

    feat: Game command contiene un byte que se envía a la testbed

--- src/server/Menu/GameCommand/RightCommand.cpp
+++ src/server/Menu/GameCommand/RightCommand.cpp
@@ -1,6 +1,6 @@
 #include "RightCommand.h"
 
-RightCommand::RightCommand(size_t id):GameCommand(id){}
+RightCommand::RightCommand(size_t id):GameCommand(id){GameCommand::num = 2+id*4;}
 void RightCommand::run(GameLogic& gameLogic) {
     gameLogic.move_player_right(id);
 }
--- src/server/Menu/GameCommand/RightReleaseCommand.cpp
+++ src/server/Menu/GameCommand/RightReleaseCommand.cpp
@@ -1,6 +1,6 @@
 #include "RightReleaseCommand.h"
 
-RightReleaseCommand::RightReleaseCommand(size_t id):GameCommand(id){}
+RightReleaseCommand::RightReleaseCommand(size_t id):GameCommand(id){GameCommand::num = id*4+0;}
 void RightReleaseCommand::run(GameLogic& gameLogic) {
-//    gameLogic.move_release_player_up(id);
+    gameLogic.brake_player(id);
 }
